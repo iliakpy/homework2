38fd069c81dcfc0a8c98ca23f87819a6c8c71ffd
iliakpy <56565312+iliakpy@users.noreply.github.com>
2019-10-23 10:24:42 +0300

    Create borndayforewer.py

---
+++ borndayforewer.py
@@ -0,0 +1,9 @@
+age_pushkin = int(input('Введи год рождения Пушкина АС: '))
+
+while age_pushkin != 1799:
+    age_pushkin = int(input('Введи год рождения Пушкина АС: '))
+
+day_pushkin = int(input('Введи дату рождения Пушкина АС: '))
+while day_pushkin != 26:
+      day_pushkin = int(input('Введи дату рождения Пушкина АС: '))
+print('Верно')
