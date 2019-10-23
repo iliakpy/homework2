a233ee49a16c91a6cbbfa2ef9af3e74c75e18542
iliakpy <56565312+iliakpy@users.noreply.github.com>
2019-10-23 10:21:24 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Описание велосипеда
+# str
+name = ('bike')
+print(type(name))
+
+name = ('Color')
+print(type(name))
+
+
+# int - целое число
+size = 19
+print(type(size))
+
+# float - число с плавающей точкой
+avg_weight = 19.5
+print(type(avg_weight))
+
+#bool - логический тип
+is_woman = True
+print(type(is_woman))
